b44f686097cb6c586d312e35f766836ba9ed758e
tfs <tiagofsanchez@gmail.com>
2024-02-09 16:10:36 +0000

    Example with a query array

--- app/page.js
+++ app/page.js
@@ -1,14 +1,30 @@
 "use client";
 import { Field, Form, Formik } from "formik";
 
+const questionsArray = [
+  {
+    code: "1",
+    question: "Something",
+  },
+  {
+    code: "2",
+    question: "Something Else",
+  },
+];
+
 export default function Home() {
+  const questionsObject = questionsArray.reduce((acc, question) => {
+    acc[question.code] = "";
+    return acc;
+  }, {});
+
+  console.log(questionsObject);
+
   return (
     <main className="min-h-screen p-24 m-2">
       <h1 className="font-3xl l">Example of yes or no question</h1>
       <Formik
-        initialValues={{
-          picked: "",
-        }}
+        initialValues={questionsObject}
         onSubmit={async (values) => {
           await new Promise((r) => setTimeout(r, 500));
           alert(JSON.stringify(values, null, 2));
@@ -18,31 +34,37 @@ export default function Home() {
           <>
             <Form className="mt-5 ">
               <div className="bg-slate-800 rounded-md p-3">
-                <div id="my-radio-group">Picked</div>
-                <div
-                  role="group"
-                  aria-labelledby="my-radio-group"
-                  className="flex gap-4"
-                >
-                  <label>
-                    <Field
-                      type="radio"
-                      name="picked"
-                      value="yes"
-                      className="mr-1"
-                    />
-                    Yes
-                  </label>
-                  <label>
-                    <Field
-                      type="radio"
-                      name="picked"
-                      value="no"
-                      className="mr-1"
-                    />
-                    No
-                  </label>
-                </div>
+                {questionsArray.map((question) => {
+                  return (
+                    <div key={question.code}>
+                      <div id="my-radio-group">{question.question}</div>
+                      <div
+                        role="group"
+                        aria-labelledby="my-radio-group"
+                        className="flex gap-4 "
+                      >
+                        <label>
+                          <Field
+                            type="radio"
+                            name={question.code}
+                            value="yes"
+                            className="mr-1"
+                          />
+                          Yes
+                        </label>
+                        <label>
+                          <Field
+                            type="radio"
+                            name={question.code}
+                            value="no"
+                            className="mr-1"
+                          />
+                          No
+                        </label>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
               <button
                 type="submit"
